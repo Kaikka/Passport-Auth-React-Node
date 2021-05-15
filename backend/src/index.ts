bb6616f8b15d1fe2a33c39a0cbc5cc8635c7750d
Kaikka <kaileeto@gmail.com>
2021-05-15 18:54:05 +0200

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import passport from "passport";
+import passportLocal from "passport-local";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import bcrypt from "bcryptjs";
+import User from "./User";
+import dotenv from "dotenv";
+import { UserInterface } from "./interfaces/UserInterface";
+
+const LocalStrategy = passportLocal.Strategy;
+
+mongoose.connect(
+  "mongodb+srv://kaikka:6gRZqh8KCfcJHrPx@cluster0.1myzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected To Mongo");
+  }
+);
+
+// Middleware
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport
+
+// Routes
+app.post("/register", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (
+    !username ||
+    !password ||
+    typeof username !== "string" ||
+    typeof password !== "string"
+  ) {
+    res.send("Wrong values!");
+    return;
+  }
+  User.findOne({ username }, async (err: Error, doc: UserInterface) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("Success, user added");
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server started");
+});
