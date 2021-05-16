486a926ad94a5018695b48e4b3e8c8f3480598cc
Kaikka <kaileeto@gmail.com>
2021-05-16 18:34:49 +0200

    User can register

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const register = () => {
+    axios
+      .post(
+        "http://localhost:4000/register",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        if (res.data === "User Already Exists") {
+          console.log(res.data);
+        }
+        if (res.data === "User registered successfully") {
+          window.location.href = "/login";
+        }
+      });
+  };
+
   return (
     <div>
-      <p>Register</p>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={register}>Register</button>
     </div>
   );
 }
