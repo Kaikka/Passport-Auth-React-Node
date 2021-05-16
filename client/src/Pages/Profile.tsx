ec41eca53422501415849422c8c789c04d49d51d
Kaikka <kaileeto@gmail.com>
2021-05-16 18:34:57 +0200

    Baseline profile page showing name

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { myContext } from "./Context";
 
 export default function Profile() {
+  const ctx = useContext(myContext);
+
   return (
     <div>
-      <p>Profile</p>
+      <h1>Currently logged in user: {ctx.username}</h1>
     </div>
   );
 }
