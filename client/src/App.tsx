b872597950477b9c0d97c278d231a9ab1fa0cff8
Kaikka <kaileeto@gmail.com>
2021-05-16 02:06:48 +0200

    browserouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import "./App.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import AdminPage from "./Pages/AdminPage";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import "./main.css";
 
 function App() {
   return (
-    <div>
-      <p>stuff</p>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Homepage}></Route>
+        <Route path="/admin" exact component={AdminPage}></Route>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/profile" exact component={Profile}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
