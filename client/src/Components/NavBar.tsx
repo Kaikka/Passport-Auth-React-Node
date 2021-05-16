57c4b6770f23cb10a9552fa51973a4ffba3c0459
Kaikka <kaileeto@gmail.com>
2021-05-16 02:08:00 +0200

    Baseline navbar

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div>
-      <p>NavBar</p>
+    <div className="NavContainer">
+      <Link to="/">Home</Link>
+      <Link to="/logout">Logout</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/admin">Admin</Link>
     </div>
   );
 }
